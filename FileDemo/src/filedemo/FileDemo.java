fd1b4ed2489a0bf4dd9aa5f907d6c7b60f65cbf4
bhoomika batham <150714831+bhoomikabatham@users.noreply.github.com>
2025-03-06 21:21:26 +0530

    Update FileDemo.java

--- FileDemo/src/filedemo/FileDemo.java
+++ FileDemo/src/filedemo/FileDemo.java
@@ -12,55 +12,49 @@ public class FileDemo {
     
     public static void main(String[] args) {
         
-        
-         File myObj = new File("new.txt"); 
-    if (myObj.delete()) { 
-      System.out.println("Deleted the file: " + myObj.getName());
-    } else {
-      System.out.println("Failed to delete the file.");
-    } 
-        
-         /*try {
-      File myObj = new File("bhoomika.txt");
-      Scanner myReader = new Scanner(myObj);
-      while (myReader.hasNextLine()) {
-        String data = myReader.nextLine();
-        System.out.println(data);
+     // Step 1: Creating a file
+       try {
+      File myObj = new File("bhoomika.txt"); // Creating a File object
+      if (myObj.createNewFile()) {   // Checking if the file does not exist
+        System.out.println("File created: " + myObj.getName());
+      } else {
+        System.out.println("File already exists.");// If file already exists
       }
-      myReader.close();
-    } catch (FileNotFoundException e) {
+    } catch (IOException e) {
       System.out.println("An error occurred.");
       e.printStackTrace();
     }
-        */
-        
-      /*  try {
-      FileWriter myWriter = new FileWriter("bhoomika.txt");
-      myWriter.write("Files in Java might be tricky, but it is fun enough!");
-      myWriter.close();
+          // Step 2: Writing to the file
+     try {
+      FileWriter myWriter = new FileWriter("bhoomika.txt"); // Creating FileWriter object
+      myWriter.write("Files in Java might be tricky, but it is fun enough!"); // Writing content to file
+      myWriter.close();// Closing FileWriter
       System.out.println("Successfully wrote to the file.");
     } catch (IOException e) {
       System.out.println("An error occurred.");
       e.printStackTrace();
     }
-        */
         
-        
-        
-        
-        
-     /*   try {
-      File myObj = new File("bhoomika.txt");
-      if (myObj.createNewFile()) {
-        System.out.println("File created: " + myObj.getName());
-      } else {
-        System.out.println("File already exists.");
+         // Step 3: Reading from the file
+         try {
+      File myObj = new File("bhoomika.txt"); // Creating a File object
+      Scanner myReader = new Scanner(myObj);// Creating Scanner object to read file
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);// Printing file content
       }
-    } catch (IOException e) {
+      myReader.close(); // Closing Scanner
+    } catch (FileNotFoundException e) {
       System.out.println("An error occurred.");
       e.printStackTrace();
-    }*/
-        
+    }
+         // Step 4: Deleting a file
+          File myObj = new File("new.txt"); // Creating a File object
+    if (myObj.delete()) { // Checking if file deletion is successful
+      System.out.println("Deleted the file: " + myObj.getName());
+    } else {
+      System.out.println("Failed to delete the file.");
+    } 
     }
     
 }
